89cfe9be59ece738b30b53181ea30d4015b32845
Victor <Victorhbarroso@hotmail.com>
2024-07-25 19:27:54 -0300

    Changed yml File once more

--- src/main/java/me/dio/UserAccount.java
+++ src/main/java/me/dio/UserAccount.java
@@ -1,6 +1,10 @@
 package me.dio;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity(name = "tb_account")
 public class UserAccount {
